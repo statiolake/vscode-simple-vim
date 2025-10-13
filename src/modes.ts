f7d7e5f949bd0df8279bf10e85f11674514db947
statiolake <statiolake@gmail.com>
2025-10-14 00:06:10 +0900

    feat: use single context variable

--- src/modes.ts
+++ src/modes.ts
@@ -5,35 +5,26 @@ import type { VimState } from './vim_state_types';
 
 export function enterInsertMode(vimState: VimState): void {
     vimState.mode = Mode.Insert;
-    setModeContext('extension.simpleVim.insertMode');
+    setModeContext('insert');
 }
 
 export function enterNormalMode(vimState: VimState): void {
     vimState.mode = Mode.Normal;
-    setModeContext('extension.simpleVim.normalMode');
+    setModeContext('normal');
 }
 
 export function enterVisualMode(vimState: VimState): void {
     vimState.mode = Mode.Visual;
-    setModeContext('extension.simpleVim.visualMode');
+    setModeContext('visual');
 }
 
 export function enterVisualLineMode(vimState: VimState): void {
     vimState.mode = Mode.VisualLine;
-    setModeContext('extension.simpleVim.visualLineMode');
+    setModeContext('visualLine');
 }
 
-function setModeContext(key: string) {
-    const modeKeys = [
-        'extension.simpleVim.insertMode',
-        'extension.simpleVim.normalMode',
-        'extension.simpleVim.visualMode',
-        'extension.simpleVim.visualLineMode',
-    ];
-
-    modeKeys.forEach((modeKey) => {
-        vscode.commands.executeCommand('setContext', modeKey, key === modeKey);
-    });
+function setModeContext(mode: 'insert' | 'normal' | 'visual' | 'visualLine') {
+    vscode.commands.executeCommand('setContext', 'simple-vim.mode', mode);
 }
 
 export function setModeCursorStyle(mode: Mode, editor: vscode.TextEditor): void {
